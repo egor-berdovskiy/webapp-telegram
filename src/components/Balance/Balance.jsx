f6dfb479ba38c412d851a10b5a760e63b65ef1a4
Your Name <you@example.com>
2023-10-15 02:54:30 +0600

    Добавтл кнопку плюс

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -4,12 +4,31 @@ import './Balance.css';
 
 import { assets } from '../../App';
 
+import Button from "../Button/Button";
+
 class Balance extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            balance: 0.0,
+            currency: '$',
+        };
+    }
+
+    componentDidMount () {
+        
+    }
+
     render() {
         return(
             <div className="balance">
-                <p id="balance-text">Баланс</p>
-                <p id="balance-value">0.0 $</p>
+                <div className="content">
+                    <p id="balance-text">Баланс</p>
+                    <div id="balance-info">
+                        <p id="balance-value">{this.state.balance} {this.state.currency}</p>
+                        <Button img={assets.plus_button} height={20} width={20} link='https://ya.ru' />
+                    </div>
+                </div>
             </div>
         )
     };
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -13,7 +13,7 @@ class Button extends React.Component {
             <div className="button">
                 <a href={this.props.link}>
                     { this.props.img ? <img src={this.props.img} width={this.props.width} height={this.props.height}></img> : null }
-                    <p>{ this.props.text }</p>
+                    { this.props.text ? <p>{ this.props.text }</p> : null }
                 </a>
             </div>
         )
