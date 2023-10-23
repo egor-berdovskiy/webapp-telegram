558b788d3f2cfbbe7aa50a550ac2856a0a74b0af
Your Name <you@example.com>
2023-10-23 19:36:29 +0600

    Поменял адрес апи

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const base_url = 'https://https://kbjq79d4-8000.euw.devtunnels.ms/';
+const base_url = 'https://6697-94-241-173-114.ngrok-free.app/';
 const api_string = 'api/v1/';
 
 const headers = {
